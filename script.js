94a8c1ac462aa42a743ba63b9d1c7c6cf66f7238
AMANJMOEZI <79466023+amanjmoezi@users.noreply.github.com>
2022-05-19 14:00:57 +0430

    add function  for make a password with script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const btnNewPass = document.querySelector("button")
 const passwordList = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ|{}[],.<>/?;:-_+=";
 let password = "";
+function makePassword() {
+    const lengthpass = Number(document.querySelector("input[type=number]").value)
+    password = "";
+    for (let i = 1; i <= lengthpass; i++) {
+        password+=passwordList[Math.floor(Math.random()*passwordList.length)]
+    }
+    console.log(password);
+}   
